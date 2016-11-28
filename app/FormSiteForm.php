a2a39e9698ab2717df55630156a0e292dcc2e8f7
Josue Morales <josue.morales.sv@gmail.com>
2016-11-28 17:44:25 -0600

    added results_limit in formisite configuration

--- app/FormSiteForm.php
+++ app/FormSiteForm.php
@@ -18,15 +18,21 @@ class FormSiteForm
 
         if(!config('formsite.base_url'))
         {
-            throw new Exception("The formsite config variable 'base_url' must be set.");
+            throw new Exception("The formsite config variable 'formsite.base_url' must be set.");
         }
         $this->base_url = config('formsite.base_url');
 
         if(!config('formsite.api_key'))
         {
-            throw new Exception("The formsite config variable 'api_key' must be set.");
+            throw new Exception("The formsite config variable 'formsite.api_key' must be set.");
         }
         $this->api_key = config('formsite.api_key');
+        
+        if(!config('formsite.results_limit'))
+        {
+            throw new Exception("The formsite config variable 'formsite.results_limit' must be set.");
+        }
+        $this->results_limit = config('formsite.results_limit');
     }
 
     public function allForms()
@@ -37,13 +43,14 @@ class FormSiteForm
     }
 
     public function getFormResults($formName, $parameters=[]){
+        
         $defaultParameters = [
             'fs_api_key'=>$this->api_key,
             'fs_page'=>1,
             'fs_sort'=>'result_id',
             'fs_sort_direction'=>'asc',
             'fs_include_headings'=>'',
-            'fs_limit'=>1   //Limit set to 1 for testing purposes
+            'fs_limit'=> $this->results_limit
         ];
         $queryParameters = array_replace($defaultParameters, $parameters);
         $url ="{$this->base_url}forms/{$formName}/results";
--- config/formsite.php
+++ config/formsite.php
@@ -2,7 +2,7 @@
 
 return [
 
-'api_key' => '0OAiofYrXYWd',
-'base_url' => 'https://fs25.formsite.com/api/users/billyg/'
-
+    'api_key' => '0OAiofYrXYWd',
+    'base_url' => 'https://fs25.formsite.com/api/users/billyg/',
+    'results_limit'=>1,
 ];
